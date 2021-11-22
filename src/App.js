c019814c51d8b34216fe51d59676e1d873e927b6
torvic <130111@unsaac.edu.pe>
2021-11-22 10:50:00 -0500

    Feat: Add increment feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			counter: 0
+		}
+	}
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={() => this.setState({counter: this.state.counter + 1})}>Incrementa</button>
       </div>
     );
   }
